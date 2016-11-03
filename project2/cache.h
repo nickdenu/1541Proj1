754e8abe4d802d7c6f575d44c0431c3f9e3f396f
Stephen Snow <scsnow54@gmail.com>
2016-11-03 13:59:52 -0400

    Partially completed necessary logic cache access

--- project2/cache.h
+++ project2/cache.h
@@ -1,6 +1,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
+
+#define WORDSIZE_BYTES 4
+#define WORDSIZE_BITS 32
 
 struct cache_blk_t {
     unsigned long tag;
@@ -19,9 +23,50 @@ struct cache_t {
     int nsets;				// # sets
     int blocksize;			// block size
     int assoc;				// associativity
+
+    int block_index_offset;
+    int block_index_mask;
+    int tag_offset;
+
     int hit_latency;			// latency in case of a hit
     struct cache_blk_t **blocks;    // the array of cache blocks
 };
+
+/**
+ * log2
+ *
+ * params: num, which is 2 raised to a positive integer
+ * returns: log2 of the number
+ */
+int log2(int num) {
+    int exponent = 0;
+
+    // continue until 1 has been shifted to 0th position
+    while ((num & 1) != 1) {
+        num = num >> 1;
+        exponent++;
+    }
+    return exponent;
+}
+
+/**
+ * ones
+ *
+ * params: n number of consecutive ones
+ * returns: an int containing the number of ones specified
+ */
+int ones(int n) {
+    int res = 0;
+    int i = 0;
+
+    while (i < n) {
+        res = res << 1;
+        res |= 1;
+        i++;
+    }
+    return res;
+}
+
 
     struct cache_t *
 cache_create(int size, int blocksize, int assoc, int latency)
@@ -31,10 +76,24 @@ cache_create(int size, int blocksize, int assoc, int latency)
     int nblocks = cachesize_bytes / blocksize;	// number of blocks in the cache
     int nsets = nblocks / assoc;			// number of sets (entries) in the cache
 
+    int words_per_block = blocksize / WORDSIZE_BYTES; 
+
+    int byte_offset = log2(WORDSIZE_BYTES);
+    int block_offset = log2(words_per_block);
+    int block_index_offset = byte_offset + block_offset;
+
+    int block_index_size = log2(nsets);
+    int block_index_mask = ones(block_index_size);
+
+    int tag_offset = block_index_size + block_offset + byte_offset;
+
     struct cache_t *C = (struct cache_t *)calloc(1, sizeof(struct cache_t));
 
     C->nsets = nsets; 
     C->assoc = assoc;
+    C->block_index_offset = block_index_offset;
+    C->block_index_mask = block_index_mask;
+    C->tag_offset = tag_offset;
     C->hit_latency = latency;
 
     C->blocks= (struct cache_blk_t **)calloc(nsets, sizeof(struct cache_blk_t));
@@ -52,7 +111,7 @@ int cache_access(struct cache_t *cp, unsigned long address,
 {
     //
     // Based on address, determine the set to access in cp and examine the blocks
-    // in the set to check hit/miss and update the golbal hit/miss statistics
+    // in the set to check hit/miss and update the global hit/miss statistics
     // If a miss, determine the victim in the set to replace (LRU). Replacement for 
     // L2 blocks should observe the inclusion property.
     //
@@ -64,6 +123,46 @@ int cache_access(struct cache_t *cp, unsigned long address,
     // It is recommended to start implementing and testing a system with only L1 (no L2). Then add the
     // complexity of an L2.
     // return(cp->hit_latency);
+
+    int latency;
+    int index, tag;
+    int column;
+    int lru = -1;
+    unsigned long long lru_ts = ULONG_MAX;
+    struct cache_blk_t *lru_block;
+    struct cache_blk_t *ablock;
+
+    index = (address >> cp->block_index_offset) && cp->block_index_mask;
+    tag = address >> tag_offset;
+
+    for (column = 0; column < c->assoc; column++) {
+        ablock = cp->blocks[index][column]; 
+
+        // check for match
+        if (ablock->valid && ablock->tag == tag) {
+            // cache hit
+            if (access_type == 'w') {
+                ablock->dirty = 1;
+            }
+            ablock->ts = now;
+            return cp->hit_latency;
+        }
+
+        // update LRU
+        if (ablock->ts < lru_ts) {
+            // this block is now the least recently used block
+            lru = column;
+            lru_ts = ablock->ts;
+            lru_block = ablock;
+        }
+    }
+
+    // getting block from next level, evicting dirty block
+    if (lru_block->dirty) {
+
+
+
     return(cp->hit_latency);
 }
+
 
