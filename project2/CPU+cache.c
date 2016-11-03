395bbab50bf409f5607f6774d123ef02138994c3
Stephen Snow <scsnow54@gmail.com>
2016-11-03 10:52:21 -0400

    Added project2 skeleton

---
+++ project2/CPU+cache.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
+#include "CPU.h" 
+
+// to keep cache statistics
+unsigned int accesses = 0;
+unsigned int read_accesses = 0;
+unsigned int write_accesses = 0;
+unsigned int L1hits = 0;
+unsigned int L1misses = 0;
+unsigned int L2hits = 0;
+unsigned int L2misses = 0;
+
+#include "cache.h" 
+
+int main(int argc, char **argv)
+{
+  struct trace_item *tr_entry;
+  size_t size;
+  char *trace_file_name;
+  int trace_view_on = 0;
+  
+  unsigned char t_type = 0;
+  unsigned char t_sReg_a= 0;
+  unsigned char t_sReg_b= 0;
+  unsigned char t_dReg= 0;
+  unsigned int t_PC = 0;
+  unsigned int t_Addr = 0;
+
+  unsigned int cycle_number = 0;
+
+  if (argc == 1) {
+    fprintf(stdout, "\nUSAGE: tv <trace_file> <switch - any character>\n");
+    fprintf(stdout, "\n(switch) to turn on or off individual item view.\n\n");
+    exit(0);
+  }
+    
+  trace_file_name = argv[1];
+  if (argc == 3) trace_view_on = atoi(argv[2]) ;
+  // here you should extract the cache parameters from the command line (cache size, associativity, latency)
+  unsigned int L1size = 16; 
+  unsigned int bsize = 32;
+  unsigned int L1assoc = 4;
+  unsigned int L2size = 0;
+  unsigned int L2assoc = 0;
+  unsigned int L2_hit_latency = 6;
+  unsigned int mem_latency = 100;
+
+  fprintf(stdout, "\n ** opening file %s\n", trace_file_name);
+
+  trace_fd = fopen(trace_file_name, "rb");
+
+  if (!trace_fd) {
+    fprintf(stdout, "\ntrace file %s not opened.\n\n", trace_file_name);
+    exit(0);
+  }
+
+  trace_init();
+  struct cache_t *L1, *L2, *nextL;
+  L1 = cache_create(L1size, bsize, L1assoc, 0); 
+  nextL = NULL;   // the next level in the hierarchy -- NULL indicates main memory
+  if (L2size != 0)
+  {
+	  L2 = cache_create(L2size, bsize, L2assoc, L2_hit_latency);
+	  nextL = L2;
+  }
+
+  while(1) {
+    size = trace_get_item(&tr_entry);
+   
+    if (!size) {       /* no more instructions (trace_items) to simulate */
+      printf("+ Simulation terminates at cycle : %u\n", cycle_number);
+	  printf("+ Cache statistics \n");
+      break;
+    }
+    else{              /* parse the next instruction to simulate */
+      cycle_number++;
+      t_type = tr_entry->type;
+      t_sReg_a = tr_entry->sReg_a;
+      t_sReg_b = tr_entry->sReg_b;
+      t_dReg = tr_entry->dReg;
+      t_PC = tr_entry->PC;
+      t_Addr = tr_entry->Addr;
+    }  
+
+// SIMULATION OF A SINGLE CYCLE cpu IS TRIVIAL - EACH INSTRUCTION IS EXECUTED
+// IN ONE CYCLE, EXCEPT IF THERE IS A DATA CACHE MISS.
+
+      switch(tr_entry->type) {
+        case ti_NOP:
+		  if (trace_view_on) printf("[cycle %d] NOP:", cycle_number);
+          break;
+        case ti_RTYPE:
+		  if (trace_view_on) {
+			printf("[cycle %d] RTYPE:", cycle_number);
+			printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->dReg);
+		  };
+          break;
+        case ti_ITYPE:
+		  if (trace_view_on){
+			printf("[cycle %d] ITYPE:", cycle_number);
+			printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+		  };
+          break;
+        case ti_LOAD:
+		  if (trace_view_on){
+			printf("[cycle %d] LOAD:", cycle_number);
+			printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+		  };
+		  accesses++;
+		  read_accesses++;
+		  cycle_number = cycle_number + cache_access(L1, tr_entry->Addr, 'r', cycle_number, nextL);
+		  break;
+        case ti_STORE:
+		  if (trace_view_on){
+			printf("[cycle %d] STORE:", cycle_number);
+			printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+		  };
+		  accesses++;
+		  write_accesses++;
+		  cycle_number = cycle_number + cache_access(L1, tr_entry->Addr, 'w', cycle_number, nextL);
+		  break;
+        case ti_BRANCH:
+		  if (trace_view_on) {
+			printf("[cycle %d] BRANCH:", cycle_number);
+			printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+		  };
+          break;
+        case ti_JTYPE:
+		  if (trace_view_on) {
+			printf("[cycle %d] JTYPE:", cycle_number);
+			printf(" (PC: %x)(addr: %x)\n", tr_entry->PC, tr_entry->Addr);
+		  };
+          break;
+        case ti_SPECIAL:
+		  if (trace_view_on) printf("[cycle %d] SPECIAL:", cycle_number);
+          break;
+        case ti_JRTYPE:
+		  if (trace_view_on) {
+			printf("[cycle %d] JRTYPE:", cycle_number);
+			printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", tr_entry->PC, tr_entry->dReg, tr_entry->Addr);
+		  };
+          break;
+      }
+   
+  }
+
+  trace_uninit();
+
+  exit(0);
+}
+
+
---
+++ project2/cache.h
@@ -0,0 +1,71 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct cache_blk_t {
+  unsigned long tag;
+  char valid;
+  char dirty;
+  unsigned long long ts;	//a timestamp that may be used to implement LRU replacement
+  // To guarantee that L2 is inclusive of L1, you may need an additional flag 
+  // in L2 to indicate that the block is cached in L1
+};
+
+struct cache_t {
+	// The cache is represented by a 2-D array of blocks. 
+	// The first dimension of the 2D array is "nsets" which is the number of sets (entries)
+	// The second dimension is "assoc", which is the number of blocks in each set.
+  int nsets;				// # sets
+  int blocksize;			// block size
+  int assoc;				// associativity
+  int hit_latency;			// latency in case of a hit
+  struct cache_blk_t **blocks;    // the array of cache blocks
+};
+
+struct cache_t *
+	cache_create(int size, int blocksize, int assoc, int latency)
+{
+  int i;
+  int nblocks = 1;			// number of blocks in the cache
+  int nsets = 1;			// number of sets (entries) in the cache
+
+  // YOUR JOB: calculate the number of sets and blocks in the cache
+  //
+  // nblocks = X;
+  // nsets = Y;
+
+  struct cache_t *C = (struct cache_t *)calloc(1, sizeof(struct cache_t));
+		
+  C->nsets = nsets; 
+  C->assoc = assoc;
+  C->hit_latency = latency;
+
+  C->blocks= (struct cache_blk_t **)calloc(nsets, sizeof(struct cache_blk_t));
+
+  for(i = 0; i < nsets; i++) {
+		C->blocks[i] = (struct cache_blk_t *)calloc(assoc, sizeof(struct cache_blk_t));
+	}
+
+  return C;
+}
+
+int cache_access(struct cache_t *cp, unsigned long address, 
+			 char access_type, unsigned long long now, struct cache_t *next_cp)
+{
+  //
+  // Based on address, determine the set to access in cp and examine the blocks
+  // in the set to check hit/miss and update the golbal hit/miss statistics
+  // If a miss, determine the victim in the set to replace (LRU). Replacement for 
+  // L2 blocks should observe the inclusion property.
+  //
+  // The function should return the hit_latency in case of a hit. In case
+  // of a miss, you need to figure out a way to return the time it takes to service 
+  // the request, which includes writing back the replaced block, if dirty, and bringing 
+  // the requested block from the lower level (from L2 in case of L1, and from memory in case of L2).
+  // This will require one or two calls to cache_access(L2, ...) for misses in L1.
+  // It is recommended to start implementing and testing a system with only L1 (no L2). Then add the
+  // complexity of an L2.
+	// return(cp->hit_latency);
+	return(cp->hit_latency);
+}
+
